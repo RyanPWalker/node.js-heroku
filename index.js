58b08a69c94b342cb4c341bda848ba2ea8109686
Ryan Walker <RyanPrestonWalker@gmail.com>
2017-11-15 17:26:15 -0700

    Fixed pg

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 var cool = require('cool-ascii-faces');
 var express = require('express');
-var pg = require('pg');
 var app = express();
-var pool = new pg.Pool()
+
+const { Pool } = require('pg');
+var pool = new Pool();
+pool.on('error', (err, client) => {
+	console.error('Unexpected error on idle client', err);
+	process.exit(-1);
+})
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -13,29 +18,30 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
-  response.render('pages/index')
+	response.render('pages/index')
 });
 
 app.get('/postal', function(request, response) {
-  response.render('pages/postal')
+	response.render('pages/postal')
 });
 
 app.get('/db', function (request, response) {
-  pool.connect(function(err, client, done) {
-    client.query('SELECT * FROM users', function(err, result) {
-      done();
-      if (err)
-       { console.error(err); response.send("Error " + err); }
-      else
-       { response.render('pages/db', {results: result.rows} ); }
-    });
-  });
+	pool.connect((err, client, done) => {
+		if (err) throw err;
+		client.query('SELECT * FROM users', [1], (err, result) => {
+			done();
+			if (err)
+				{ console.error(err); response.send("Error " + err); }
+			else
+				{ response.render('pages/db', {results: result.rows} ); }
+		});
+	});
 });
 
 app.get('/cool', function(request, response) {
-  response.send(cool());
+	response.send(cool());
 });
 
 app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+	console.log('Node app is running on port', app.get('port'));
 });
