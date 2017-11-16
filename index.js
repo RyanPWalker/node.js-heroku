a89b9dcbe617c9fa1cd49fb8185e3a56ffe8b9e8
Ryan Walker <RyanPrestonWalker@gmail.com>
2017-11-15 17:33:20 -0700

    Fixed pg

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ var express = require('express');
 var app = express();
 
 const { Pool } = require('pg');
-var pool = new Pool();
+var pool = new Pool({
+	database: 'process.env.DATABASE_URL',
+});
 pool.on('error', (err, client) => {
 	console.error('Unexpected error on idle client', err);
 	process.exit(-1);
