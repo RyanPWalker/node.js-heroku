8d77ce63501c9c4d64072ba27159c06ba318dbcf
Ryan Walker <RyanPrestonWalker@gmail.com>
2017-12-09 23:33:11 -0700

    Debugging insety query

--- dbhandler.js
+++ dbhandler.js
@@ -1,5 +1,12 @@
 module.exports = (request) => {
 	return new Promise(function(resolve,reject) {
+		const { Client } = require('pg');
+		const client = new Client({
+		  connectionString: process.env.DATABASE_URL,
+		  ssl: true,
+		});
+		client.connect();
+		
 		var bcrypt 				= require('bcrypt');
 		const saltRounds 		= 10;
 		var email 				= request.body.email;
