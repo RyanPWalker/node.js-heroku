d30f54ccaf4e28de0edf2794dd1265118ad776a3
Ryan Walker <RyanPrestonWalker@gmail.com>
2017-12-09 22:39:27 -0700

    Working on create profile

--- dbhandler.js
+++ dbhandler.js
@@ -1,16 +1,26 @@
 module.exports = (request) => {
 
-	var params = url.parse(request.url, true);
-	var action = params.query.action;
-	console.log(action);
+	var bcrypt 				= require('bcrypt');
+	const saltRounds 		= 10;
+	var email 				= request.body.email;
+	var password 			= request.body.password;
+	var name				= request.body.name;
+	var age					= request.body.age;
+	console.log(email + password + name + age);
 
-	if (action = 'selectmessages') {
-		client.query('SELECT * FROM posts LIMIT 50', (err, result) => {
-			if (err)
-				{ console.error(err); response.json("Error " + err); }
-			else
-				{ response.json(result.rows); }
-			client.end();
-		});
-	}
+	client.query('INSERT INTO users ()', (err, result) => {
+		if (err)
+			{ console.error(err); response.json("Error " + err); }
+		else
+			{ response.json(result.rows); }
+		client.end();
+	});
+
+	/*client.query('SELECT * FROM posts LIMIT 50', (err, result) => {
+		if (err)
+			{ console.error(err); response.json("Error " + err); }
+		else
+			{ response.json(result.rows); }
+		client.end();
+	});*/
 };
