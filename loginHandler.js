12888e42d94a352ee23a69131e29441324362bc6
Ryan Walker <RyanPrestonWalker@gmail.com>
2017-12-10 21:00:15 -0700

    debugging

--- loginHandler.js
+++ loginHandler.js
@@ -22,7 +22,7 @@ module.exports = (request) => {
 				console.error("Error " + err); resolve(); 
 			}
 			else { 
-				var hash = String(result);
+				var hash = String(result.row.password);
 				console.log(hash);
 				bcrypt.compare(password, hash, function(err, res) {
 					if (err) {
